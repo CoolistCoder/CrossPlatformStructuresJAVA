3fd504c2339117e474a74234ab55100c3ba2c630
Alec Roberts <22121853+CoolistCoder@users.noreply.github.com>
2025-08-14 11:07:47 -0500

    250814JAVA LinkedList redefined as abstract

--- CrossPlatformStructuresJAVA/src/dataStructures/LinkedList.java
+++ CrossPlatformStructuresJAVA/src/dataStructures/LinkedList.java
@@ -1,5 +1,13 @@
 package dataStructures;
+//Author's note: Comments added to eliminate ambiguity
 
-public class LinkedList {
-
+public abstract class LinkedList<T> {
+	//All linked lists composed of nodes
+	protected class Node{
+		private T data;		//data contained within node
+		private Node next;	//Node next to this one
+		private Node prev;	//Node prior to this one (not singly)
+	}
+	
+	public abstract int size(); //Retrieves the number of nodes in the list
 }
