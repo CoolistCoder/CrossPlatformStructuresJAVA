a7b057285d0f80d1083d0226313c7e49ddda9e92
Alec Roberts <22121853+CoolistCoder@users.noreply.github.com>
2025-08-14 11:13:14 -0500

    250814JAVA Node class getters/setters added

--- CrossPlatformStructuresJAVA/src/dataStructures/LinkedList.java
+++ CrossPlatformStructuresJAVA/src/dataStructures/LinkedList.java
@@ -7,6 +7,32 @@ public abstract class LinkedList<T> {
 		private T data;		//data contained within node
 		private Node next;	//Node next to this one
 		private Node prev;	//Node prior to this one (not singly)
+		
+		//getters
+		T getData() {
+			return this.data;
+		}
+		
+		Node getNext() {
+			return this.next;
+		}
+		
+		Node getPrev() {
+			return this.prev;
+		}
+		
+		//setters
+		void setData(T newData) {
+			this.data = newData;
+		}
+		
+		void setNext(Node newNext) {
+			this.next = newNext;
+		}
+		
+		void setPrev(Node newPrev) {
+			this.prev = newPrev;
+		}
 	}
 	
 	public abstract int size(); //Retrieves the number of nodes in the list
